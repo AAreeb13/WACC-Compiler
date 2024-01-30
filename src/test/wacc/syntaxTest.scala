090d9cf0feb6fc01a2d6c060a77288f363b1edca
Areeb <aaa1322@ic.ac.uk>
2024-01-30 03:02:47 +0000

    feat: [aaa1322,iu22] Add tests to match expr with atom

--- src/test/wacc/syntaxTest.scala
+++ src/test/wacc/syntaxTest.scala
@@ -18,13 +18,28 @@ class lexerIdentifierTest extends AnyFlatSpec {
     }
 
     "An ident" should "not start with a digit" in {
-        lexer.ident.parse("1").isFailure
-        lexer.ident.parse("9sx").isFailure
+        lexer.ident.parse("1").isFailure shouldBe true
+        lexer.ident.parse("9sx").isFailure shouldBe true
     }
     "An ident" should "be able to contain digits, characters or underscore in between" in {
         lexer.ident.parse("a283") shouldBe Success("a283")
         lexer.ident.parse ("alpha") shouldBe Success("alpha")
         lexer.ident.parse("__42__") shouldBe Success("__42__")
     }
+}
 
+class parserExpressionTest extends AnyFlatSpec {
+    "An expr" should "match an atom" in {
+        parser.expr.parse("1") shouldBe Success(IntVal(1))
+        parser.expr.parse("\'c\'") shouldBe Success(CharVal('c'))
+        parser.expr.parse("\"Hello Mahdi Ahmed\"") shouldBe Success(StrVal("Hello Mahdi Ahmed"))
+        parser.expr.parse("variable") shouldBe Success(Var("variable"))
+        //parser.expr.parse("arr[3]") shouldBe Success(ArrayVal("arr", IntVal(3) :: Nil))
+        parser.expr.parse("((((\"hello\"))))") shouldBe Success(StrVal("hello"))
+        parser.expr.parse("arr[true]").isFailure shouldBe true
+        parser.expr.parse("null") shouldBe Success(PairVal)
+        parser.expr.parse("true") shouldBe Success(BoolVal(true))
+        parser.expr.parse("false") shouldBe Success(BoolVal(false))
+        parser.expr.parse("((((\"hello\"))))") shouldBe Success(StrVal("hello"))
+    }
 }
