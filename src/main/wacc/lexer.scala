ca5bd9b5b8d9cba71819b7cbaad7ca50ec57e9e1
Mahdi <ma3722@ic.ac.uk>
2024-01-30 12:00:37 +0000

    feat: add identifier for lexer

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -1,16 +1,22 @@
 package wacc
 
 import parsley.Parsley
+import parsley.token.{Lexer, predicate}
 import parsley.token.Lexer
 import parsley.token.descriptions._
 
 object lexer {
     private val desc = LexicalDesc.plain.copy(
-        // your configuration goes here
+        nameDesc = NameDesc.plain.copy(
+            identifierStart = predicate.Basic(c => c.isLetter || c == '_'),
+            identifierLetter = predicate.Basic(c => c.isLetterOrDigit || c == '_')
+        ),
+        spaceDesc = SpaceDesc.plain
     )
     private val lexer = new Lexer(desc)
 
     val integer = lexer.lexeme.integer.decimal
+    val ident: Parsley[String] = lexer.lexeme.names.identifier
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
