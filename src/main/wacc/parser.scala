d33ea70b9417d439f1d444479c7244d591f7a6d3
Mahdi <ma3722@ic.ac.uk>
2024-02-01 17:42:20 +0000

    fix: parsing of prefix unary ops are atomic

--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -14,13 +14,37 @@ object parser {
     def parse[Err: ErrorBuilder](input: String): Result[Err, Prog] = parser.parse(input)
     
     lazy val parser = fully(prog)
+    
+    lazy val types 
+        = baseType |
+        arrType |
+        pairType
+
+    lazy val baseType 
+        = (IntType from "int") |
+        (CharType from "char") |
+        (StrType from "string") |
+        (BoolType from "bool")
+
+    lazy val arrType: Parsley[Type]
+        = atomic(chain.postfix1(baseType | pairType)(ArrType from "[" <~> "]"))
+
+    lazy val pairType
+        = PairType(atomic("pair" ~> "(") ~>  pairElemType, "," ~> pairElemType <~ ")")
+
+    lazy val pairElemType: Parsley[Type] 
+        = baseType |
+        arrType |
+        (ErasedPair from "pair")
+    
     lazy val prog = "begin" ~> Prog(many(func), stmtList) <~ "end"
+
 
     lazy val exprs = many(expr)
     
     lazy val expr: Parsley[Expr] = 
         precedence(atom)(
-            Ops(Prefix)(Not from "!", Neg from "-", Len from "len", Ord from "ord", Chr from "chr"),
+            Ops(Prefix)(Not from "!", Neg from "-", Len from atomic("len"), Ord from atomic("ord"), Chr from atomic("chr")),
             Ops(InfixL)(Div from "/", Mul from "*", Mod from "%"), 
             Ops(InfixL)(Add from "+", Sub from "-"),
             Ops(InfixN)(atomic(GrtEql from ">="), Grt from ">", atomic(LessEql from "<="), Less from "<"),
@@ -41,29 +65,8 @@ object parser {
         StrVal(strLiter)| 
         "(" ~> expr <~ ")"    
 
-    lazy val types 
-        = baseType |
-        arrType |
-        pairType
+
     
-    lazy val baseType 
-        = (IntType from "int") |
-        (CharType from "char") |
-        (StrType from "string") |
-        (BoolType from "bool")
-
-    lazy val arrType: Parsley[Type]
-        = precedence[Type](baseType, pairType)(
-            Ops(Postfix)(ArrType from "[" <~> "]")
-        )
-
-    lazy val pairType
-        = PairType(("pair" ~> "(") ~>  pairElemType, "," ~> pairElemType <~ ")")
-
-    lazy val pairElemType: Parsley[Type] 
-        = baseType |
-        arrType |
-        (ErasedPair from "pair")
 
     lazy val func: Parsley[Func] = Func(types, ident, "(" ~> paramList <~ ")", "is" ~> stmtList <~ "end") 
 
@@ -81,7 +84,7 @@ object parser {
         Exit("exit" ~> expr) |
         Print("print" ~> expr) |
         Println("println" ~> expr) |
-        If("if" ~> expr, "then" ~> stmtList, "else" ~> stmtList <~ "fi")
+        If("if" ~> expr, "then" ~> stmtList, "else" ~> stmtList <~ "fi") |
         While("while" ~> expr, "do" ~> stmtList <~ "done") |
         Scope("begin" ~> stmtList <~ "end") 
 
