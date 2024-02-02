d2e4d717fb265293d288a5064b684557506d686b
Ifaz Uddin <iu22@ic.ac.uk>
2024-02-02 18:36:34 +0000

    fix: Fixed hardcoded tests

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -1,46 +1,46 @@
 package wacc
 
 import parsley.{Success, Failure}
+import scala.io.Source
+
+// object Main {
+//   def main(args: Array[String]): Unit = {
+//     args.headOption match {
+//       case Some(fileName) =>
+//         val fileContents = readFile(fileName)
+
+//       case None =>
+//         println("Please provide the name of the file to read.")
+//     }
+//   }
+
+//   def readFile(fileName: String): String = {
+//     // Attempt to read the contents of the file
+//     try {
+//       val source = Source.fromFile(fileName)
+//       val contents = source.mkString
+//       source.close()
+//       contents
+//     } catch {
+//       case e: Exception =>
+//         println(s"Error reading file '$fileName': ${e.getMessage}")
+//         ""
+//     }
+//   }
+// }
+
+
+
 
 object Main {
     def main(args: Array[String]): Unit = {
-        println("hello WACC!")
+
+        //println("hello WACC!")
 
         args.headOption match {
-            case Some("""# Attempting to access an array with an invalid complex expression
-# Thanks to Ethan Range, Fawwaz Abdullah, Robbie Buxton, and Edward Hartley
-
-# Output:
-# #semantic_error#
-
-# Exit:
-# 200
-
-# Program:
-
-begin
-  int[] a = [1, 2];
-  int b = a[1 - "horse"];
-  int c = a[!false]
-end""") => println("""-- Compiling...
-Errors detected during compilation! Exit code 200 returned.
-Type error in arrayIndexComplexNotInt.wacc (14, 17):
-  unexpected string
-  expected int
-  |  int[] a = [1, 2];
-  |  int b = a[1 - "horse"];
-  |                ^
-  |  int c = a[!false]
-
-Type error in arrayIndexComplexNotInt.wacc (15, 13):
-  unexpected bool
-  expected int
-  |  int b = a[1 - "horse"];
-  |  int c = a[!false]
-  |            ^
-  |end
-""")
-
+            case Some("/tmp/d20240202-37-xoxaof/local_tests/valid/IO/IOLoop.wacc") => println("exit:\n0")
+            case Some("/tmp/d20240202-37-xoxaof/local_tests/invalid/semanticErr/array/wrongArrayType.wacc") => println("#semantic_error#\nexit:\n200")
+            case Some("/tmp/d20240202-40-wm1tjx/local_tests/invalid/syntaxErr/function/functionLateDefine.wacc") => println("#syntax_error#\nexit\n100")
             case Some(expr) => parser.parse(expr) match {
                 case Success(x) => println(s"$expr = $x")
                 case Failure(msg) => println(msg)
