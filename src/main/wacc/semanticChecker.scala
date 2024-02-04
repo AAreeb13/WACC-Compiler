b7b23cfdf2e8cda7aeecb54906b9da898381ce60
Aahil Jouher <aj1722@ic.ac.uk>
2024-02-04 10:20:43 +0000

    feat: create Analyser class to access globals (in semanticChecker) easier

--- src/main/wacc/semanticChecker.scala
+++ src/main/wacc/semanticChecker.scala
@@ -1,15 +1,29 @@
 package wacc
 
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.ListBuffer
 
 object semanticChecker {
     def verify(result: Either[String, Node]): Either[String, Node] = result.flatMap(_ match {
-        case prog: Prog =>
-            // Semantic checks will go here
-            var globalTable = new SymbolTable();
-            return result
+        case prog: Prog => Analyser(prog).getResult
         case _ => Left("Invalid AST type for top-level verification")
     })
+}
+
+case class Analyser(val prog: Prog) {
+    var errList: ListBuffer[String] = ListBuffer.empty
+    var globalTable = new SymbolTable();
+    def getResult: Either[String, Node] = {
+        if (errList.isEmpty) {
+            Right(prog)
+        } else {
+            Left(generateErrors)
+        }
+    }
+
+    def generateErrors: String = {
+        "A semantic error occured"
+    }
 }
 
 class SymbolTable(val parent: Option[SymbolTable] = None) {
