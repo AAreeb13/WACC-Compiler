19092511b2be45996ff5c64239794eaac9508dd6
Aahil Jouher <aj1722@ic.ac.uk>
2024-02-04 09:37:20 +0000

    feat: add symbol table class

--- src/main/wacc/semanticChecker.scala
+++ src/main/wacc/semanticChecker.scala
@@ -1,10 +1,34 @@
 package wacc
 
+import scala.collection.mutable.HashMap
+
 object semanticChecker {
     def verify(result: Either[String, Node]): Either[String, Node] = result.flatMap(_ match {
         case prog: Prog =>
             // Semantic checks will go here
+            var globalTable = new SymbolTable();
             return result
         case _ => Left("Invalid AST type for top-level verification")
     })
+}
+
+class SymbolTable(val parent: Option[SymbolTable] = None) {
+    var table: HashMap[String, Type] = parent match {
+        case None => HashMap()
+        case Some(value) => value.table.clone() // shallow copy
+    }
+    
+    def addAll(xs: IterableOnce[(String, Type)]) = 
+        table.addAll(xs)
+
+    def addOne(name: String, declType: Type) =
+        table.addOne(name, declType)
+
+    def contains(name: String) =
+        table.contains(name)
+    
+    def typeof(name: String) = table.get(name)
+
+    // maybe add some flatten method so we can easily get all the info
+    // in the global symbol table
 }
