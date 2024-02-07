b072bc0a14eb9ba587e2e013e8ec849c2a075e99
Aahil Jouher <aj1722@ic.ac.uk>
2024-02-07 13:04:16 +0000

    feat: add test cases for each test type in wacc examples

--- src/test/wacc/integration.scala
+++ src/test/wacc/integration.scala
@@ -2,5 +2,24 @@ package wacc.integration
 
 import org.scalatest.flatspec.AnyFlatSpec
 import wacc._
+class IntegrationTest extends AnyFlatSpec {
+    val exitSuccess     = 0
+    val exitSyntaxErr   = 100
+    val exitSemanticErr = 200
+    val syntaxCheckOnly = true
 
-class IntegrationTest extends AnyFlatSpec {}
+    "Valid examples" should "compile successfully" in {
+        runTests("wacc_examples/valid", exitSuccess) 
+    }
+
+    "Syntax error examples" should "produce a syntax error" in {
+        runTests("wacc_examples/invalid/syntaxErr", exitSyntaxErr) 
+    }
+
+    "Semantic error examples" should "produce a semantic error" in {
+        runTests("wacc_examples/invalid/semanticErr", exitSemanticErr) 
+    }
+
+    def runTests(path: String, expected: Int): Unit = {
+    }
+}
